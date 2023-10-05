c1e3c48977b03fc9bf33a4ab25430551c2e12e3c
Vanilna <oleksandra.vytiahlovska@monterail.com>
2023-10-05 18:11:38 +0200

    Fix typo in app name

--- android/app/src/main/java/com/harrypotterlego/MainActivity.java
+++ android/app/src/main/java/com/harrypotterlego/MainActivity.java
@@ -1,4 +1,4 @@
-package com.harrypoterlego;
+package com.harrypotterlego;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
@@ -14,7 +14,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "HarryPoterLego";
+    return "HarryPotterLego";
   }
 
   /**
--- android/app/src/main/java/com/harrypotterlego/MainApplication.java
+++ android/app/src/main/java/com/harrypotterlego/MainApplication.java
@@ -1,4 +1,4 @@
-package com.harrypoterlego;
+package com.harrypotterlego;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
